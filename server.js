4fcc0b7575c83cbae82673ebea0792b79d5cd147
aueh0548580842-hue <aueh0548580842@gmail.com>
2026-03-07 23:09:02 +0200

    Refactor IVR handler to use destructuring for query params

--- server.js
+++ server.js
@@ -2,43 +2,23 @@ const express = require('express');
 const app = express();
 
 app.get('/ivr-handler', (req, res) => {
-    const idSelection = req.query.id_selection;
-    const phoneToDial = req.query.phone_to_dial;
+    const { id_selection, phone_to_dial } = req.query;
 
-    // שלב א: בקשת מספר זיהוי (כוכבית = אקראי, סולמית = חסוי)
-    if (!idSelection) {
-        return res.send(`read=t-נא הקש מספר לזיהוי. לזיהוי אקראי הקש כוכבית. לזיהוי חסוי הקש סולמית בלבד=id_selection,no,15,1,7,#,yes,0123456789*#`);
+    // שלב 1: בקשת מספר זיהוי
+    if (!id_selection) {
+        return res.send(`read=t-נא להקיש מספר לזיהוי, לזיהוי אקראי הקש כוכבית, לזיהוי חסוי הקש סולמית=id_selection,no,15,1,7,#,yes,0123456789*#`);
     }
 
-    // שלב ב: אישור הבחירה ובקשת מספר היעד
-    if (!phoneToDial) {
-        let msg = "";
-        if (idSelection.includes("*")) {
-            msg = "בחרת בזיהוי אקראי. ";
-        } else if (idSelection === "#" || idSelection === "") {
-            msg = "בחרת בזיהוי חסוי. ";
-        } else {
-            msg = `הזיהוי שנבחר הוא ${idSelection.split('').join(' ')}. `;
-        }
-
-        // חשוב: אנחנו שולחים את id_selection הלאה כדי שהחיוג ידע מה בחרת
-        return res.send(`read=t-${msg}נא הקש מספר יעד ובסיום סולמית=phone_to_dial,no,15,1,15,#,yes,0123456789&id_selection=${idSelection}`);
+    // שלב 2: בקשת מספר יעד (העברת id_selection הלאה כדי למנוע ניתוק)
+    if (!phone_to_dial) {
+        let msg = id_selection.includes('*') ? "אקראי" : (id_selection === "#" ? "חסוי" : id_selection.split('').join(' '));
+        return res.send(`read=t-זיהוי ${msg}. נא להקיש מספר יעד וסולמית=phone_to_dial,no,15,1,15,#,yes,0123456789&id_selection=${id_selection}`);
     }
 
-    // שלב ג: ביצוע החיוג הסופי
-    let callerId = "";
-    if (idSelection.includes("*")) {
-        callerId = "rand";
-    } else if (idSelection === "#" || idSelection === "") {
-        callerId = "private";
-    } else {
-        callerId = idSelection;
-    }
-
-    res.send(`routing_number=${phoneToDial}&set_caller_id=${callerId}`);
+    // שלב 3: חיוג סופי
+    let callerId = id_selection.includes('*') ? "rand" : (id_selection === "#" ? "private" : id_selection);
+    res.send(`routing_number=${phone_to_dial}&set_caller_id=${callerId}`);
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
