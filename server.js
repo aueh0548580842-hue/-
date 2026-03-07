05e25e732af5c5c50302f888a99700b10c60e5ff
aueh0548580842-hue <aueh0548580842@gmail.com>
2026-03-07 22:56:48 +0200

    Refactor IVR handler for clarity and consistency

--- server.js
+++ server.js
@@ -5,26 +5,35 @@ app.get('/ivr-handler', (req, res) => {
     const idSelection = req.query.id_selection;
     const phoneToDial = req.query.phone_to_dial;
 
-    // שלב א: אם המשתמש רק נכנס ואין עדיין בחירת זיהוי
+    // שלב א: בקשת מספר זיהוי (כוכבית = אקראי, סולמית = חסוי)
     if (!idSelection) {
-        return res.send(`read=t-נא הקש מספר לזיהוי, לכוכבית הקש כוכבית, ולזיהוי חסוי הקש סולמית=id_selection,no,15,1,7,#,yes,0123456789*#`);
+        return res.send(`read=t-נא הקש מספר לזיהוי. לזיהוי אקראי הקש כוכבית. לזיהוי חסוי הקש סולמית בלבד=id_selection,no,15,1,7,#,yes,0123456789*#`);
     }
 
-    // שלב ב: אם יש זיהוי אבל אין מספר יעד
+    // שלב ב: אישור הבחירה ובקשת מספר היעד
     if (!phoneToDial) {
         let msg = "";
-        if (idSelection === "*") msg = "בחרת זיהוי אקראי. ";
-        else if (idSelection === "#") msg = "בחרת זיהוי חסוי. ";
-        else msg = `הזיהוי הוא ${idSelection.split('').join(' ')}. `;
+        if (idSelection.includes("*")) {
+            msg = "בחרת בזיהוי אקראי. ";
+        } else if (idSelection === "#" || idSelection === "") {
+            msg = "בחרת בזיהוי חסוי. ";
+        } else {
+            msg = `הזיהוי שנבחר הוא ${idSelection.split('').join(' ')}. `;
+        }
 
-        return res.send(`read=t-${msg}נא הקש את מספר היעד ובסיום סולמית=phone_to_dial,no,15,1,7,#,yes,0123456789&id_selection=${idSelection}`);
+        // חשוב: אנחנו שולחים את id_selection הלאה כדי שהחיוג ידע מה בחרת
+        return res.send(`read=t-${msg}נא הקש מספר יעד ובסיום סולמית=phone_to_dial,no,15,1,15,#,yes,0123456789&id_selection=${idSelection}`);
     }
 
-    // שלב ג: יש את כל הנתונים - מבצעים חיוג
+    // שלב ג: ביצוע החיוג הסופי
     let callerId = "";
-    if (idSelection === "*") callerId = "rand";
-    else if (idSelection === "#") callerId = "private";
-    else callerId = idSelection;
+    if (idSelection.includes("*")) {
+        callerId = "rand";
+    } else if (idSelection === "#" || idSelection === "") {
+        callerId = "private";
+    } else {
+        callerId = idSelection;
+    }
 
     res.send(`routing_number=${phoneToDial}&set_caller_id=${callerId}`);
 });
