c6d3f342379c1250cf7137444e437a94fd7a1d2d
aueh0548580842-hue <aueh0548580842@gmail.com>
2026-04-14 16:42:04 +0300

    Replace Express server with PHP script

--- server.js
+++ server.js
@@ -1,35 +1,12 @@
-const express = require('express');
-const app = express();
+<?php
+// 1. אם המשתמש הקיש מספר, נגדיר אותו כזיהוי החדש ונשלח אותו לשלוחה הבאה
+if (isset($_GET['ApiVal'])) {
+    $new_id = $_GET['ApiVal'];
+    // הפקודה אומרת למערכת: קבע את המשתנה ApiPhone למספר החדש ועבור לשלוחה 1
+    echo "set_var=ApiPhone=$new_id&go_to_folder=/1";
+    exit;
+}
 
-app.get('/my-api', (req, res) => {
-    // הדפסה ל-Logs של Render כדי שתוכל לראות מה מגיע מהמערכת
-    console.log("--- קריאה חדשה התקבלה ---");
-    console.log("נתונים שהתקבלו:", req.query);
-
-    const callerId = req.query.ApiEnterID_1;
-    const destination = req.query.ApiEnterID_2;
-
-    // שלב 1: בקשת מספר זיהוי
-    if (!callerId) {
-        console.log("סטטוס: חסר מספר זיהוי. שולח בקשת הקשה.");
-        return res.send("read=t-נא הקש מספר זיהוי ובסיום סולמית=chosen_id,yes,10,3,10,No,yes,no");
-    }
-
-    // שלב 2: בקשת מספר יעד
-    if (!destination) {
-        console.log("סטטוס: יש זיהוי (" + callerId + "), חסר מספר יעד. שולח בקשת הקשה.");
-        return res.send("read=t-נא הקש מספר יעד ובסיום סולמית=dest_id,yes,10,3,10,No,yes,no");
-    }
-
-    // שלב 3: ביצוע החיוג
-    console.log(`סטטוס: מבצע חיוג! זיהוי: ${callerId}, יעד: ${destination}`);
-    
-    // הפקודה המלאה לחיוג
-    res.send(`routing_caller_id=${callerId}&go_to_address=api_phone_dial=${destination}`);
-});
-
-// הגדרת פורט תואמת ל-Render
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-    console.log(`השרת רץ בהצלחה על פורט ${PORT}`);
-});
+// 2. אם המשתמש רק נכנס, נבקש ממנו להקיש את המספר הרצוי
+echo "read=t-נא הקישו את מספר הזיהוי החדש לסיום הקישו סולמית=num,yes,1,1,10,Digits,no,yes";
+?>
